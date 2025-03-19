8ee3dfe8280a3c691e4c7ec17d97c7d7a4e7f73f
mcclellandj <59489169+mcclellandj@users.noreply.github.com>
2025-03-18 19:00:59 -0500

    Add files via upload

---
+++ twitter_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Twitter access codes:
+# API Key
+API_key = 'aXIwriliBxQB9P8TfpOHBSakA'
+# API Key Secret
+API_key_secret = 'NptaYAxYC6wjkK8hUTQMmxNTleXhUQ1QIzUF42yeZiBfMVyQpP'
+# Access Token
+Access_Token = '1355456276-AKAyNSuG7hJpVW75F2unPQAIQoDDhxfQ9xjwBk4'
+# Access Token Secret
+Access_Token_secret = 'ohAKJbVgyoBx8eoQKGnotthupKf9tdjMg4acmNeeZCqrJ'
